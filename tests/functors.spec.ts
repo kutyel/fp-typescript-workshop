171025cdb020fbb19f202842311067e91f46756f
Flavio Corpa <flaviocorpa@gmail.com>
2023-10-03 14:43:49 +0200

    Add exercise about safely parsing numbers!

--- tests/functors.spec.ts
+++ tests/functors.spec.ts
@@ -1,5 +1,10 @@
-import { Option, pipe, String } from 'effect'
+import { Option, pipe, String, Predicate } from 'effect'
 import { expect, test, describe } from 'bun:test'
+
+interface User {
+  readonly id: number
+  readonly name: string
+}
 
 describe('Functors', () => {
   // Exercise 1
@@ -11,7 +16,7 @@ describe('Functors', () => {
 
   // Exercise 2
   test('Implement head to safely get the first element of a list.', () => {
-    // BONUS: Option.fromIterable
+    // BONUS POINTS: Option.fromIterable
     const head = <A>(xs: ReadonlyArray<A>): Option.Option<A> =>
       xs.length > 0 ? Option.some(xs[0]) : Option.none()
     const xs = ['do', 'ray', 'me', 'fa', 'so', 'la', 'si', 'do']
@@ -20,11 +25,7 @@ describe('Functors', () => {
   })
 
   // Exercise 3
-  test('Use map/flatMap and String.at(0) to find the first initial of the user.', () => {
-    interface User {
-      readonly id: number
-      readonly name: string
-    }
+  test(`Use map/flatMap and String to return the first letter of the user's name.`, () => {
     const user: User = { id: 2, name: 'Albert' }
     // HINT: String.at(0) :: string -> Option<string>
     const initial = (user?: User) =>
@@ -35,5 +36,20 @@ describe('Functors', () => {
       )
     expect(initial(user)).toEqual(Option.some('A'))
     expect(initial()).toEqual(Option.none())
+  })
+
+  // Exercise 4
+  test('Use Option to refactor `safeNum` without an if statement.', () => {
+    // const safeNum = function(n) {
+    //   if (n) {
+    //     return Number(n)
+    //   }
+    // }
+    // BONUS POINTS: Option.liftPredicate
+    // MOAR BONUS POINTS: Predicate.not
+    // safeNum :: string -> Option<number>
+    const safeNum = (n: string) => pipe(n, Number, Option.liftPredicate(Predicate.not(isNaN)))
+    expect(safeNum('4')).toEqual(Option.some(4))
+    expect(safeNum('foo')).toEqual(Option.none())
   })
 })
