89e8c77b33d652fa44604b30cbdc5f79d2d082c9
Flavio Corpa <flaviocorpa@gmail.com>
2023-10-05 11:44:04 +0200

    Add exercise 2 with monads reading from the filesystem! 🙉

--- tests/monads.spec.ts
+++ tests/monads.spec.ts
@@ -1,5 +1,6 @@
-import { Option, pipe } from 'effect'
+import { Option, pipe, Effect, String, ReadonlyArray } from 'effect'
 import { expect, test, describe } from 'bun:test'
+import { sep } from 'path'
 
 import { User, EmptyObj } from '..'
 
@@ -24,8 +25,24 @@ describe('Monads', () => {
         Option.flatMap((u) => Option.fromNullable(u.address)),
         Option.map((a) => a.street.name)
       )
+
     expect(getStreetName()).toEqual(Option.none())
     expect(getStreetName({})).toEqual(Option.none())
     expect(getStreetName(user)).toEqual(Option.some('Walnut St'))
+  })
+
+  // Exercise 2
+  test("Use getFile to get the filename, remove the directory so it's just the file, then log it.", async () => {
+    // getFile :: Effect<never,never,string>
+    const getFile = Effect.succeed(import.meta.path)
+    // HINT: String.split, ReadonlyArray.last
+    // logFilename :: Effect<never,NoSuchElementException,string>
+    const logFilename = pipe(
+      getFile,
+      Effect.flatMap((x) => pipe(x, String.split(sep), ReadonlyArray.last))
+    )
+
+    const result = await Effect.runSync(logFilename)
+    expect(result).toBe('monads.spec.ts')
   })
 })
